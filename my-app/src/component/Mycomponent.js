76c8a658dc368353694a72ae6cda6ae7e9cac147
minhtuphan1804 <157300546+minhtuphan1804@users.noreply.github.com>
2024-04-04 09:29:26 +0700

    day2

--- my-app/src/component/Mycomponent.js
+++ my-app/src/component/Mycomponent.js
@@ -4,6 +4,11 @@ import React from 'react';
 class myComponent extends React.Component {
     //JSX
 
+    state = {
+        name: 'Phan Minh Tu',
+        age: 12,
+        address: 'Ha Noi'
+    };
     render() {
         var nb = Math.random();//cach1
         return (
@@ -11,6 +16,8 @@ class myComponent extends React.Component {
                 {nb}//cach1
                 {Math.random()}
                 //cach2
+
+                My name is {this.state.name} and i'm from {this.state.address}
 
             </div>
         );
